2dcaa71768571c11f3a5e4bce01266c67171ff04
Joel Ling <joel-ling@protonmail.com>
2023-03-02 15:30:27 +0900

    add pkg. containing source of random FQDNs

---
+++ pkg/sources/uuid-source.go
@@ -0,0 +1,36 @@
+package sources
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+type uuidSource struct {
+	domain string
+}
+
+func NewUUIDSource(domain string) (s *uuidSource) {
+	s = &uuidSource{
+		domain: domain,
+	}
+
+	return
+}
+
+func (s *uuidSource) GenerateFQDN() (fqdn string) {
+	const (
+		format = "%s.%s."
+	)
+
+	fqdn = fmt.Sprintf(format,
+		generateUUID(),
+		s.domain,
+	)
+
+	return
+}
+
+func generateUUID() string {
+	return uuid.New().String()
+}
---
+++ pkg/sources/uuid-source_test.go
@@ -0,0 +1,49 @@
+package sources
+
+import (
+	"regexp"
+	"testing"
+)
+
+const (
+	uuidPattern = `[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`
+)
+
+func TestGenerateUUID(t *testing.T) {
+	var (
+		random string
+		valid  bool
+	)
+
+	random = generateUUID()
+
+	valid, _ = regexp.MatchString(uuidPattern, random)
+
+	if !valid {
+		t.Fail()
+	}
+}
+
+func TestUUIDSourceGenerateFQDN(t *testing.T) {
+	const (
+		domain  = "example.org"
+		pattern = uuidPattern + `\.` + domain + `\.`
+	)
+
+	var (
+		source *uuidSource
+
+		random string
+		valid  bool
+	)
+
+	source = NewUUIDSource(domain)
+
+	random = source.GenerateFQDN()
+
+	valid, _ = regexp.MatchString(pattern, random)
+
+	if !valid {
+		t.Fail()
+	}
+}
